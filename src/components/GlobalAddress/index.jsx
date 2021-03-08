6950f5a8407ff0eab8c730e5f62ac5cd5daaf06a
Phong <phongduytran7999@gmail.com>
2021-03-09 01:16:44 +0700

    build global address search

---
+++ src/components/GlobalAddress/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { HiLocationMarker } from 'react-icons/hi'
+import { FaAngleRight } from 'react-icons/fa'
+import './style.scss'
+
+function GlobalAddress() {
+    return (
+        <div className="global-address-search">
+            <span className="global-address__title">Giao tới địa chỉ</span>
+            <div className="global-address__content">
+                <HiLocationMarker className="icon-location"/>
+                <div className="content">Số 8 ngõ 33 Kiều Sơn, Đằng Lâm, Hải An, Hải Phòng, Việt Nam</div>
+                <FaAngleRight className="icon-right"/>
+            </div>
+        </div>
+    )
+}
+
+export default GlobalAddress
--- src/features/UserApp/pages/Main/index.jsx
+++ src/features/UserApp/pages/Main/index.jsx
@@ -2,6 +2,7 @@ import Navbar from "components/Navbar";
 import React from "react";
 import "assets/css/grid.css";
 import Banner from "components/Banner";
+import GlobalAddress from "components/GlobalAddress"
 
 function MainPage(props) {
   return (
@@ -12,7 +13,9 @@ function MainPage(props) {
       <div className="grid__full-width">
         <Banner/>
       </div>
-      
+      <div className="grid wide">
+        <GlobalAddress />
+      </div>
     </div>
   );
 }
