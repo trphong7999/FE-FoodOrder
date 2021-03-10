c6261dd2fa721f06d93ed29f19dd6d01c9581bd2
unknown <phong75664@st.vimaru.edu.vn>
2021-03-10 19:11:34 +0700

    footer hompage

--- src/features/UserApp/pages/Main/index.jsx
+++ src/features/UserApp/pages/Main/index.jsx
@@ -1,19 +1,22 @@
 import Navbar from "components/Navbar";
 import React, {useState} from "react";
 import "assets/css/grid.css";
+import "./index.scss";
 import Banner from "components/Banner";
 import GlobalAddress from "components/GlobalAddress"
 import SlickList from "features/UserApp/components/Slide";
 import Newfeed from "features/UserApp/components/Newfeed";
+import Footer from "features/UserApp/components/Footer";
 
 export default function MainPage() {
   const [SlideImage, setSlideImage] = useState([])
   
   return (
-    <div>
+    <div className="main-page">
       <div className="grid wide">
         <Navbar />
       </div>
+      <div className="bg-w">
       <div className="grid__full-width">
         <Banner/>
       </div>
@@ -25,6 +28,10 @@ export default function MainPage() {
       </div>
       <div className="grid wide">
         <Newfeed />
+      </div>
+      </div>
+      <div className="grid wide">
+        <Footer/>
       </div>
     </div>
   );
