0ac0b90cc0ca9ceee7a509018348ae656eb80ee7
Phong <phongduytran7999@gmail.com>
2021-03-24 22:00:25 +0700

    chore: handle change currentAddress in form search address

--- src/features/UserApp/components/GlobalAddress/index.jsx
+++ src/features/UserApp/components/GlobalAddress/index.jsx
@@ -1,25 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import { HiLocationMarker } from "react-icons/hi";
 import { FaAngleRight } from "react-icons/fa";
 import "./style.scss";
-import FormAddressSearch from "../ModalProfile/FormAddressSearch";
+import FormAddressSearch from "./FormAddressSearch/FormAddressSearch";
+import Modal from "react-modal";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    padding: "0",
+  },
+  overlay: {
+    backgroundColor: "rgba(0,0,0,0.4)",
+  },
+};
 
 function GlobalAddress(props) {
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {}
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+  // ----------------------------------Map---------------
+  const [location, setLocation] = useState({
+    address: "",
+    currentAddress:
+      "Số 8 ngõ 33 Kiều Sơn, Đằng Lâm, Hải An, Hải Phòng, Việt Nam",
+    lat: "20.828790101307185",
+    lng: "106.71664668177716",
+  });
+
   return (
-    <div className="global-address-search">
+    <div id="yourAppElement" className="global-address-search">
       <section className="grid wide">
-        <div className="global-address__wrap">
+        <div className="global-address__wrap" onClick={openModal}>
           <span className="global-address__title">Giao tới địa chỉ</span>
           <div className="global-address__content">
             <HiLocationMarker className="icon-location" />
-            <div className="content">
-              Số 8 ngõ 33 Kiều Sơn, Đằng Lâm, Hải An, Hải Phòng, Việt Nam
-            </div>
+            <div className="content">{location.currentAddress}</div>
             <FaAngleRight className="icon-right" />
           </div>
         </div>
       </section>
-      {/* <FormAddressSearch /> */}
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <FormAddressSearch
+          closeModal={closeModal}
+          location={location}
+          setLocation={setLocation}
+        />
+      </Modal>
     </div>
   );
 }
---
+++ src/features/UserApp/components/PlaceAutoComplete/index.jsx
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import PlacesAutocomplete from "react-places-autocomplete";
+import {
+  geocodeByAddress,
+  geocodeByPlaceId,
+  getLatLng,
+} from "react-places-autocomplete";
+
+function PlaceAutocomplete({ location, setLocation }) {
+  const handleChange = (address) => {
+    setLocation({ ...location, address: address });
+  };
+
+  const handleSelect = (address) => {
+    geocodeByAddress(address)
+      .then((results) => {
+        console.log(results);
+        return getLatLng(results[0]);
+      })
+      .then((latLng) => {
+        const { lat, lng } = latLng;
+        setLocation({
+          ...location,
+          lat: lat,
+          lng: lng,
+          address: address,
+        });
+      })
+      .catch((error) => console.error("Error"));
+  };
+
+  return (
+    <PlacesAutocomplete
+      value={location.address}
+      onChange={handleChange}
+      onSelect={handleSelect}
+      onClick={handleChange}
+      style={{ width: "100%" }}
+    >
+      {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+        <div className="input_address">
+          <input
+            {...getInputProps({
+              placeholder: "nhập địa chỉ của bạn",
+              className: "location-search-input",
+            })}
+            name="address"
+            required
+          />
+          <div
+            className="autocomplete-dropdown-container"
+            style={{ position: "absolute", zIndex: 1000, width: "57%" }}
+          >
+            {loading && <div>Loading...</div>}
+            {suggestions.map((suggestion) => {
+              const className = suggestion.active
+                ? "suggestion-item--active"
+                : "suggestion-item";
+              // inline style for demonstration purpose
+              const style = suggestion.active
+                ? {
+                    backgroundColor: "#fafafa",
+                    cursor: "pointer",
+                    fontSize: "1.5rem",
+                    padding: "1rem",
+                  }
+                : {
+                    backgroundColor: "#ffffff",
+                    cursor: "pointer",
+                    fontSize: "1.5rem",
+                    padding: "1rem",
+                  };
+              return (
+                <div
+                  {...getSuggestionItemProps(suggestion, {
+                    className,
+                    style,
+                  })}
+                >
+                  <span>{suggestion.description}</span>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      )}
+    </PlacesAutocomplete>
+  );
+}
+
+export default PlaceAutocomplete;
