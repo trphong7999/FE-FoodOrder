8778342f0d4cee2d48ef4f0aefe5299dbbbcbc5f
Hieu <hieu73600@st.vimaru.edu.vn>
2021-04-21 22:34:00 +0700

    add useEffect UserApp: merchant

--- src/features/UserApp/pages/MerchantPage/index.jsx
+++ src/features/UserApp/pages/MerchantPage/index.jsx
@@ -1,16 +1,37 @@
 import Navbar from "features/UserApp/components/Navbar";
-import React from "react";
+import React, { useEffect } from "react";
 import Footer from "features/UserApp/components/Footer";
 import Brand from "features/UserApp/components/Brand";
 import CartOrder from "features/UserApp/components/CartOrder";
 
 import "./style.scss";
+import { useRouteMatch } from "react-router";
+import merchantApi from "api/merchantApi";
 
 export default function MerchantPage() {
+  const match = useRouteMatch();
+
+  useEffect(() => {
+    const fetchMerchant = async () => {
+      try {
+        // const params = {
+        //   _page: 1,
+        //   _limit: 10,
+
+        // };
+        const res = await merchantApi.get(match.params.id);
+        console.log(res);
+      } catch (error) {
+        console.log("Failed to fetch product list: ", error);
+      }
+    };
+
+    fetchMerchant();
+  }, []);
   return (
     <div className="merchant__main">
       <Navbar />
-      
+
       <div className="merchant__content">
         <section className="grid wide">
           <div className="row">
