2dd63a70bb642bcd03d51ff288e21859bd788f5a
Phong <phongduytran7999@gmail.com>
2021-05-30 22:48:01 +0700

    features: add navigation feature for navbar menu mobile of UserApp

--- src/features/UserApp/components/Navbar/index.jsx
+++ src/features/UserApp/components/Navbar/index.jsx
@@ -8,10 +8,13 @@ import ModalLogin from "features/UserApp/components/ModalLogin";
 import NavAccount from "features/UserApp/components/Navbar/NavAccount";
 
 import { GiHamburgerMenu } from "react-icons/gi";
-import { MdClear } from "react-icons/md";
-import { FaSignInAlt } from "react-icons/fa";
+import { MdClear, MdAssignmentInd } from "react-icons/md";
+import { FaSignInAlt, FaRedRiver } from "react-icons/fa";
 import { RiUserAddFill } from "react-icons/ri";
-import { useSelector } from "react-redux";
+import { MdAssignment } from "react-icons/md";
+import { GoSignIn } from "react-icons/go";
+import { logout } from "redux/loginUserAppSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 function Navbar(props) {
   const user = useSelector((state) => state.loginUserApp);
@@ -21,6 +24,7 @@ function Navbar(props) {
   const handleShowMenuMobile = () => {
     setShowMenu(!showMenu);
   };
+  console.log(user);
   // --------------------------------
   return (
     <nav className="navbar">
@@ -61,22 +65,35 @@ function Navbar(props) {
                 }
               >
                 <div className="content__head">
-                  <img src={logo} alt="logo" className="content__head-logo" />
+                  {user.username ? (
+                    <img
+                      src={user.profile.info.avt}
+                      alt="avatar-user"
+                      className="content__head-avt"
+                    />
+                  ) : (
+                    <img src={logo} alt="logo" className="content__head-logo" />
+                  )}
+                  <span className="content__head-text">
+                    {user.username ? user.profile.info.name : ""}
+                  </span>
                   <MdClear
                     className="content__head-icon"
                     onClick={handleShowMenuMobile}
                   />
                 </div>
-                <ul className="content__list">
-                  <li className="content__list-item">
-                    <FaSignInAlt className="content__list-icon" />
-                    <span className="content__list-span">Đăng nhập</span>
-                  </li>
-                  <li className="content__list-item">
-                    <RiUserAddFill className="content__list-icon" />
-                    <span className="content__list-span">Đăng ký</span>
-                  </li>
-                </ul>
+                {user.username ? (
+                  <ListMenuMobile />
+                ) : (
+                  <ul className="content__list">
+                    <li className="content__list-item">
+                      <FaSignInAlt className="content__list-icon" />
+                      <span className="content__list-span">
+                        <ModalLogin />
+                      </span>
+                    </li>
+                  </ul>
+                )}
               </div>
             </div>
           </div>
@@ -89,3 +106,38 @@ function Navbar(props) {
 }
 
 export default Navbar;
+
+function ListMenuMobile() {
+  const dispatch = useDispatch();
+  const handleLogout = (e) => {
+    e.preventDefault();
+    dispatch(logout());
+  };
+
+  return (
+    <ul className="content__list">
+      <li>
+        <Link to="/user/tai-khoan" className="content__list-item">
+          <MdAssignmentInd className="content__list-icon" />
+          <span className="content__list-span">Hồ sơ của tôi</span>
+        </Link>
+      </li>
+      <li>
+        <Link to="/user/dang-den" className="content__list-item">
+          <FaRedRiver className="content__list-icon" />
+          <span className="content__list-span">Theo dõi đơn hàng</span>
+        </Link>
+      </li>
+      <li>
+        <Link to="/user/tai-khoan" className="content__list-item">
+          <MdAssignment className="content__list-icon" />
+          <span className="content__list-span">Lịch sử đơn hàng</span>
+        </Link>
+      </li>
+      <li className="content__list-item" onClick={(e) => handleLogout(e)}>
+        <GoSignIn className="content__list-icon" />
+        <span className="content__list-span">Đăng xuất</span>
+      </li>
+    </ul>
+  );
+}
