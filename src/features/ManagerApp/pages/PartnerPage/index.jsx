4fd44569ed572f1a63ca01bb6852e2d94ff746eb
Phong <phongduytran7999@gmail.com>
2021-06-05 23:11:04 +0700

    features: render history order of partner in manager app

--- src/features/ManagerApp/pages/PartnerPage/index.jsx
+++ src/features/ManagerApp/pages/PartnerPage/index.jsx
@@ -9,6 +9,10 @@ import avtDefault from "assets/image/avartar/slide1.jpg";
 import reviewApi from "api/review";
 import "./style.scss";
 import merchantApi from "api/orderApi";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
 
 export default function PartnerPage() {
   let { id } = useParams();
@@ -70,9 +74,9 @@ export default function PartnerPage() {
                 {taskBar === 1 ? (
                   <Profile partner={partner} />
                 ) : taskBar === 3 ? (
-                  <Review partner={partner}/>
+                  <Review partner={partner} />
                 ) : (
-                  <HistoryOrder partner={partner}/>
+                  <HistoryOrder partner={partner} />
                 )}
               </div>
             </div>
@@ -159,20 +163,21 @@ function Review({ partner }) {
 
   useEffect(() => {
     const getAllReviewByMer = async () => {
-      const res = await reviewApi.getReviewByMerId({id: partner._id, type: 2});
+      const res = await reviewApi.getReviewByMerId({
+        id: partner._id,
+        type: 2,
+      });
       res.reverse();
       setAllReview(res);
-      console.log(res)
+      console.log(res);
     };
     getAllReviewByMer();
   }, []);
 
-  
-
   return (
-    <div className="task-shop">
-      <div className="brand-review">
-        {allReview.map((review, idx) => (
+    <div className="brand-review">
+      {allReview.length > 0 ? (
+        allReview.map((review, idx) => (
           <div key={idx} className="brand-reviewm__item">
             <div className="item-head">
               <div className="item-head__left">
@@ -209,25 +214,280 @@ function Review({ partner }) {
             </div>
             <div className="item-body">
               <span>{review.text}</span>
-              
             </div>
           </div>
-        ))}
-      </div>
+        ))
+      ) : (
+        <h1>Hiện không có đánh giá nào</h1>
+      )}
     </div>
   );
 }
 
-function HistoryOrder({partner}) {
-  const [historyOrder, setHistoryOrder] = useState([])
+function HistoryOrder({ partner }) {
+  const [historyOrder, setHistoryOrder] = useState([]);
 
   useEffect(() => {
     const getHistoryOrder = async () => {
-      const res = await merchantApi.getOrderByPartner(partner._id)
-      setHistoryOrder(res)
-    }
-    getHistoryOrder()
-  }, [])
-  console.log(historyOrder)
-  return(<div>hello</div>)
+      const res = await merchantApi.getOrderByPartner(partner._id);
+      setHistoryOrder(res);
+    };
+    getHistoryOrder();
+  }, []);
+
+  console.log(historyOrder);
+  return (
+    <div className="history-order__list">
+      {historyOrder.length > 0 ? (
+        <table className="table-order">
+          <thead>
+            <tr>
+              <th>STT</th>
+              <th>Mã đơn hàng</th>
+              <th>Thời gian</th>
+              <th>Cửa hàng</th>
+              <th>Khách hàng</th>
+              <th>Tổng tiền</th>
+              <th>Thu nhập</th>
+              <th>Trạng thái</th>
+              <th>Chi tiết</th>
+            </tr>
+          </thead>
+          {historyOrder.map((o, idx) => (
+            <tbody>
+              <tr key={idx}>
+                <td>{idx + 1}</td>
+                <td>{o._id}</td>
+                <td>
+                  <div>Thời gian đặt</div>
+                  <div style={{ marginBottom: "1rem" }}>
+                    {formatDatetimeToString(new Date(+o.timeOrder))}
+                  </div>
+                  <div>Thời gian giao</div>
+                  <div>
+                    {o.timeDeliverDone
+                      ? formatDatetimeToString(new Date(+o.timeDeliverDone))
+                      : "Không có"}
+                  </div>
+                </td>
+                <td>
+                  <div style={{ marginBottom: "1rem" }}>
+                    {o.merchantId.name}
+                  </div>
+                  <div>{o.merchantId.location.address}</div>
+                </td>
+                <td>
+                  <div
+                    style={{ marginBottom: "1rem", textTransform: "uppercase" }}
+                  >
+                    {o.userOrderId.info.name}
+                  </div>
+                  <div>{o.userOrderId.info.phone}</div>
+                </td>
+                <td>
+                  <div>{validatePrice(o.detail.total + o.detail.fee)} đ</div>
+                </td>
+                <td>
+                  <div style={{ color: "green" }}>
+                    + {validatePrice(o.detail.fee * 0.9)} đ
+                  </div>
+                </td>
+                <td>
+                  {o.status === "complete" ? (
+                    <div
+                      style={{
+                        backgroundColor: "green",
+                        color: "white",
+                        padding: "1rem 0",
+                        borderRadius: "8px",
+                        margin: "0.5rem",
+                      }}
+                    >
+                      Đã giao
+                    </div>
+                  ) : (
+                    <div
+                      style={{
+                        color: "white",
+                        padding: "1rem 0",
+                        borderRadius: "8px",
+                        margin: "0.5rem",
+                        backgroundColor: "red",
+                      }}
+                    >
+                      Đã hủy
+                    </div>
+                  )}
+                </td>
+                <td>
+                  {o.status === "complete" ? (
+                    <Chat order={o} />
+                  ) : (
+                    <div
+                      reasonCancel={o.reasonCancel}
+                      style={{ marginBottom: "1rem", cursor: "pointer" }}
+                    >
+                      Lý do hủy
+                    </div>
+                  )}
+
+                  <DetailFood order={o} />
+                </td>
+              </tr>
+            </tbody>
+          ))}
+        </table>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(1, 1, 1),
+    outline: "none",
+  },
+}));
+
+function Chat({ order }) {
+  const { chat, deliverId } = order;
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className="item__chat">
+      <span onClick={handleOpen}>Lịch sử chat</span>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <div>
+              {chat.length > 0
+                ? chat.map((chat, index) =>
+                    chat.type == 0 ? (
+                      <div key={index} className="content-parter">
+                        <img
+                          src={order.deliverId.avt}
+                          alt="partnerAvt"
+                          width="32"
+                          height="32"
+                        />
+                        <div className="content-parter__text">
+                          {chat.content}
+                        </div>
+                      </div>
+                    ) : (
+                      <div key={index} className="content-user">
+                        <div className="content-user__text">{chat.content}</div>
+                        <img
+                          src={order.userOrderId.info.avt}
+                          alt="userAvt"
+                          width="32"
+                          height="32"
+                        />
+                      </div>
+                    )
+                  )
+                : ""}
+            </div>
+          </div>
+        </Fade>
+      </Modal>
+    </div>
+  );
+}
+
+function DetailFood({ order }) {
+  const { detail } = order;
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div className="item__dishes">
+      <span onClick={handleOpen}>Chi tiết món</span>
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        className={classes.modal}
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500,
+        }}
+      >
+        <Fade in={open}>
+          <div className={classes.paper}>
+            <div className="dishes-detail__manager">
+              <div className="list-dishes">
+                <div className="dish">
+                  <div className="dish-name">Tên món</div>
+                  <div className="dish-quantity">SL</div>
+                  <div className="dish-total">Thành tiền</div>
+                </div>
+                {detail.foods.map((dish, idx) => (
+                  <div className="dish" key={idx}>
+                    <div className="dish-name">{dish.name}</div>
+                    <div className="dish-quantity">x {dish.quantity}</div>
+                    <div className="dish-total">
+                      {validatePrice(dish.total)} đ
+                    </div>
+                  </div>
+                ))}
+              </div>
+              <div className="amount">
+                <span>Tổng </span>
+                {validatePrice(detail.total)} đ
+              </div>
+              <div className="amount">
+                <span>Ship </span>
+                {validatePrice(detail.fee)} đ
+              </div>
+              <div className="amount">
+                <span>Tổng chi phí </span>
+                {validatePrice(detail.total + detail.fee)} đ
+              </div>
+            </div>
+          </div>
+        </Fade>
+      </Modal>
+    </div>
+  );
 }
