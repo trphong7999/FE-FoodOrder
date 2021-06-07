d66ac7244ec8f99ee8553d7a54df1788b54c7849
Hieu <hieu73600@st.vimaru.edu.vn>
2021-06-08 01:59:46 +0700

    change socket

--- src/socket-io.js
+++ src/socket-io.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 const id =
   localStorage.merchantId || localStorage.partnerId || localStorage.userId;
 const type = localStorage.type;
-const socket = io(`localhost:4000?id=${id}&type=${type}`, {
+const socket = io(`${process.env.REACT_SOCKET_API}?id=${id}&type=${type}`, {
   transport: ["websocket"],
 });
 
