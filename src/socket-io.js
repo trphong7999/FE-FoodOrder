962749638468c5174dc406dfcb611c4696006a35
Hieu <hieu73600@st.vimaru.edu.vn>
2021-06-08 02:09:51 +0700

    fix

--- src/socket-io.js
+++ src/socket-io.js
@@ -3,8 +3,11 @@ import io from "socket.io-client";
 const id =
   localStorage.merchantId || localStorage.partnerId || localStorage.userId;
 const type = localStorage.type;
-const socket = io(`${process.env.REACT_SOCKET_API}?id=${id}&type=${type}`, {
-  transport: ["websocket"],
-});
+const socket = io(
+  `https://api-food-order-thesis.herokuapp.com?id=${id}&type=${type}`,
+  {
+    transport: ["websocket"],
+  }
+);
 
 export default socket;
